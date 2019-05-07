488007eb8b3befde0ed089f316f3d0f2265bd35d
xunzou <xunzou@gmail.com>
2019-05-07 18:23:23 +0800

    修改.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from './layouts/header'
+// import Header from './layouts/header'
 import Login from './views/login'
 import Home from './views/home'
 import About from './views/about'
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Switch>
           <Route path="/" exact component={Login} />
           <Route path="/home" component={Home} />
--- src/views/login.js
+++ src/views/login.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
-
+import { Link } from "react-router-dom";
 class Login extends Component {
   render() {
     return (
-      <h2>Login</h2>
+      <>
+        <h2>Login</h2>
+        <form>
+          <ul>
+            <li>
+              用户名：
+              <input type="text" />
+            </li>
+            <li>
+              密  码：
+              <input type="password" />
+            </li>
+            <li>
+            <input type="submit" value="Submit" />
+            </li>
+          </ul>
+        </form>
+        <Link to="/home/">Home</Link>
+      </>
     )
   } 
 }
